4d2237815431aec4473fbfb25cf077699176878b
chirimof <chirimof@gmail.com>
2018-11-07 23:04:16 +0900

    Add filter test and group test

--- src/adapters/group.rs
+++ src/adapters/group.rs
@@ -21,3 +21,36 @@ pub trait ChooseGroup: AsSqlParts
         Having::new(self, conditions)
     }
 }
+
+#[cfg(test)]
+mod group_test {
+    use super::*;
+    use ::dev::*;
+    setup_table!({
+        namespace: users,
+        columns: [{id: Id, name: Name, email: Email}],
+        primary: Id
+    });
+
+    #[test]
+    fn group_by_test() {
+        // Incorrect statement, but it is ok because it is construction test.
+        let expected = "SELECT users.id, users.name, users.email FROM users \
+        GROUP BY users.id";
+
+        let parts = users::Table.select(users::All)
+            .group_by(users::Id);
+        assert_eq!(parts.as_sql_parts(), expected);
+    }
+
+    #[test]
+    fn having_test() {
+        let expected = "SELECT users.id, users.name, users.email FROM users \
+        GROUP BY users.id HAVING users.id > ?";
+
+        let parts = users::Table.select(users::All)
+            .group_by(users::Id)
+            .having(users::Id.greater());
+        assert_eq!(parts.as_sql_parts(), expected);
+    }
+}
